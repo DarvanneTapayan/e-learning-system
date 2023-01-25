4c996cb66085e8647b6fb5225935fda9ad7d166c
Dee Starling <38714205+DarvanneTapayan@users.noreply.github.com>
2023-01-25 18:34:45 +0800

    updated

--- php_includes/sign-up.inc.php
+++ php_includes/sign-up.inc.php
@@ -1,1 +1,42 @@
 <?php
+
+if (isset($_POST["submit"])) {
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdrepeat = $_POST["pwdrepeat"];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if (emptyInputSignup($name, $email, $username, $pwd, $pwdrepeat) !== false) {
+        header("location: ../sign-up.php?error=emptyinput");
+        exit();
+    }
+    if (invalidUid($username) !== false) {
+        header("location: ../sign-up.php?error=emptyuid");
+        exit();
+    }
+    if (invalidEmail($email) !== false) {
+        header("location: ../sign-up.php?error=emptyemail");
+        exit();
+    }
+    if (pwdMatch($pwd, $pwdRepeat) !== false) {
+        header("location: ../sign-up.php?error=passwordsdontmatch");
+        exit();
+    }
+    if (uidExists($conn, $username, $email) !== false) {
+        header("location: ../sign-up.php?error=passwordsdontmatch");
+        exit();
+    }
+
+    createUser($conn, $name, $email, $username, $pwd);
+
+
+}
+else {
+    header("location: ../sign-up.php");
+    exit();
+}
