357944c984054205304c7f9ac353f251a9ad4a1b
Dee Starling <38714205+DarvanneTapayan@users.noreply.github.com>
2023-01-25 15:46:09 +0800

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -1,4 +1,4 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="UTF-8" />
@@ -45,7 +45,7 @@
 			</section>
 		</main>
 	</body>
-</html> -->
+</html>
 						<!-- <div class="sign-up-two">
 						<p>Remember me</p>
 						<p class="password">Forget password?</p>
